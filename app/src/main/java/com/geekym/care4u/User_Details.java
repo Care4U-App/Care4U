2505568f72aaf79a0678b3e172783764adf24f94
Binay Shaw <binayshaw7777@gmail.com>
2021-10-29 20:08:49 +0530

    Button + Themes Added

--- app/src/main/java/com/geekym/care4u/User_Details.java
+++ app/src/main/java/com/geekym/care4u/User_Details.java
@@ -9,6 +9,9 @@ public class User_Details extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_user_details);
+
+
     }
 }
