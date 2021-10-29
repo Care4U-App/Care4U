54fdb0ec95734a19cde0b4dc0ab49a58b6cce1b8
Aritra Das <aritrarick2002@gmail.com>
2021-10-29 20:43:24 +0530

    Splash Screen and Onboarding Screen Done 🚀

--- app/src/main/java/com/geekym/care4u/Splashscreen.java
+++ app/src/main/java/com/geekym/care4u/Splashscreen.java
@@ -2,13 +2,58 @@ package com.geekym.care4u;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
+import android.window.SplashScreen;
 
 public class Splashscreen extends AppCompatActivity {
+
+    SharedPreferences onBoardingScreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_splashscreen);
+
+        ConnectivityManager connectivityManager = (ConnectivityManager)
+                getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        if (networkInfo==null || !networkInfo.isConnected() || !networkInfo.isAvailable()){
+
+            Toast.makeText(Splashscreen.this, "Not Connected to internet!", Toast.LENGTH_SHORT).show();
+        }
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                onBoardingScreen = getSharedPreferences("onBoardingScreen",MODE_PRIVATE);
+                boolean isFirstTime = onBoardingScreen.getBoolean("firstTime",true);
+
+                if (isFirstTime){
+
+                    SharedPreferences.Editor editor = onBoardingScreen.edit();
+                    editor.putBoolean("firstTime",false);
+                    editor.commit();
+
+                    Intent i=new Intent(Splashscreen.this, OBS.class);
+                    startActivity(i);
+                    finish();
+
+                }else{
+                    Intent i=new Intent(Splashscreen.this, Homescreen.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        }, 3000);
     }
 }
